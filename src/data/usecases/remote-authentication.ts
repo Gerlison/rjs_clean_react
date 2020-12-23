80d4c48317b337e364898b9b9765ea22b7487513
Gerlison <franciscojerlison1@gmail.com>
2020-12-23 15:27:42 -0300

    feat: add remote-authentication usecase

---
+++ src/data/usecases/remote-authentication.ts
@@ -0,0 +1,12 @@
+import { HttpPostClient } from '../protocols/http/HttpPostClient'
+
+export class RemoteAuthentication {
+  constructor (
+    private readonly url: string,
+    private readonly httpPostClient: HttpPostClient
+  ) {}
+
+  async auth (): Promise<void> {
+    return await this.httpPostClient.post(this.url)
+  }
+}
